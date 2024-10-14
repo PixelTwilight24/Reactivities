244a28319c4cd5bee5e8417a123ec4fe8aa25137
Philip Andrew Ferrer <ferrerphilipandrew@gmail.com>
2024-10-14 16:45:57 +0800

    persistUpdatedAndCreatedDataToDB

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,37 +1,57 @@
-import React from "react";
-import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
+
+import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import { SyntheticEvent, useState } from "react";
 
 interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean
 
 }
 
-export default function ActivityList({activities, selectActivity, deleteActivity}: Props){
-    return(
-        <Segment>
-            <Item.Group divided>
-                {activities.map(activity => (
-                    <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as={'a'}> {activity.title} </Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                <div>{activity.description}</div>
-                                <div>{activity.city}, {activity.venue}</div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)} floated="right" content="view" color="blue"></Button>
-                                <Button onClick={() => deleteActivity(activity.id)} floated="right" content="delete" color="red"></Button>
-                                
-                                <Label basic content={activity.category} />
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
-                ))}
-            </Item.Group>
-        </Segment>
-    )
-}
+export default function ActivityList({
+    activities, 
+    selectActivity, 
+    deleteActivity,
+    submitting}: Props){
+
+        const [targetButton, setTargetButton] = useState('');
+
+        function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+            setTargetButton(e.currentTarget.name);
+            deleteActivity(id);
+        }
+
+        return(
+            <Segment>
+                <Item.Group divided>
+                    {activities.map(activity => (
+                        <Item key={activity.id}>
+                            <Item.Content>
+                                <Item.Header as={'a'}> {activity.title} </Item.Header>
+                                <Item.Meta>{activity.date}</Item.Meta>
+                                <Item.Description>
+                                    <div>{activity.description}</div>
+                                    <div>{activity.city}, {activity.venue}</div>
+                                </Item.Description>
+                                <Item.Extra>
+                                    <Button onClick={() => selectActivity(activity.id)} floated="right" content="view" color="blue"></Button>
+                                    <Button 
+                                        name={activity.id}
+                                        loading={submitting && targetButton === activity.id} // This will only show loading on the button that was click
+                                        onClick={(e) => handleActivityDelete(e, activity.id)}   
+                                        floated="right" 
+                                        content="delete" 
+                                        color="red"></Button>
+                                    
+                                    <Label basic content={activity.category} />
+                                </Item.Extra>
+                            </Item.Content>
+                        </Item>
+                    ))}
+                </Item.Group>
+            </Segment>
+        )
+    }   
